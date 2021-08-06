f5819b77a77067c3c556d6af3a622ce85933953f
Duy <sleepynguyener@gmail.com>
2021-08-06 14:25:03 -0400

    Add parser

---
+++ data/venue_parser.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+venues_csv = pd.DataFrame(pd.read_csv('Amsterdam-places-en.sample.csv', usecols=['name', 'rating','latitude', 'longitude', 'state', 'categories', 'address', 'opening_hours_note', 'phone', 'email', 'description_text', 'official_link', 'media_landscape', 'media_portrait'], error_bad_lines=False))
+venues_csv.to_json("amsterdam.json", orient="records", date_format="epoch", double_precision=10, force_ascii=True, date_unit="ms", default_handler= None)
+
+print(venues_csv)
