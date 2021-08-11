d21e1f0b8be9cdea3f502a0e877dd9128bb0cfb9
Duy Nguyen <sleepynguyener@gmail.com>
2021-08-11 17:41:39 -0400

    Recursive read, parse and write

--- data/venue_parser.py
+++ data/venue_parser.py
@@ -1,6 +1,12 @@
+from glob import glob
 import pandas as pd
 
-venues_csv = pd.DataFrame(pd.read_csv('Amsterdam.csv', usecols=['name', 'rating','latitude', 'longitude', 'state', 'categories', 'address', 'opening_hours_note', 'phone', 'email', 'description_text', 'official_link', 'media_landscape', 'media_portrait'], error_bad_lines=False))
-venues_csv.to_json("amsterdam.json", orient="records", date_format="epoch", double_precision=10, force_ascii=True, date_unit="ms", default_handler= None)
+csv_files = glob("csv/*.csv")
 
-print(venues_csv)
+dfList_venues = [pd.DataFrame(pd.read_csv(file, usecols=['name', 'rating', 'latitude', 'longitude', 'state', 'categories', 'address', 'opening_hours_note',
+                       'phone', 'email', 'description_text', 'official_link', 'media_landscape', 'media_portrait'], error_bad_lines=False)) for file in csv_files]
+
+for (idx, df) in enumerate(dfList_venues):
+    df.to_json("json/{}.json".format(idx), orient="records", date_format="epoch",
+                      double_precision=10, force_ascii=True, date_unit="ms", default_handler=None)
+    print(df)
